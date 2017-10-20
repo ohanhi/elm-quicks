9199a1b47426ad9aa8e21f3c7c20635ba3a655bd
ohanhi <ossi.hanhinen@gmail.com>
2017-10-20 15:29:11 +0300

    Split string into just letters

--- day-01/Main.elm
+++ day-01/Main.elm
@@ -1,7 +1,40 @@
 module Main exposing (..)
 
+import Char
 import Html
 
 
 main =
-    Html.text "Hello Elm"
+    Html.text (toString (isPalindrome palindrome))
+
+
+palindrome =
+    "Are we not drawn onward, we few, drawn onward to new era?"
+
+
+isPalindrome input =
+    let
+        characters =
+            String.toList (String.toLower input)
+
+        justLetters =
+            List.filter
+                (\currentChar -> List.member currentChar aToZ)
+                characters
+
+        check =
+            Debug.log "the letters" justLetters
+    in
+    True
+
+
+asciiCodeLowercaseA =
+    97
+
+
+asciiCodeLowercaseZ =
+    122
+
+
+aToZ =
+    List.map Char.fromCode (List.range asciiCodeLowercaseA asciiCodeLowercaseZ)

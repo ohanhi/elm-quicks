76d93c6a27f195d050157b810856f1f54bf668f9
ohanhi <ossi.hanhinen@gmail.com>
2017-11-03 15:44:56 +0200

    Allow storing palindromes in model

--- day-03/Main.elm
+++ day-03/Main.elm
@@ -14,20 +14,54 @@ main =
         }
 
 
-update textThatTheUserHasWritten model =
-    textThatTheUserHasWritten
+type Msg
+    = UpdateCurrentPalindrome String
+    | SavePalindrome
+
+
+update msg model =
+    case msg of
+        UpdateCurrentPalindrome text ->
+            { model | currentPalindome = text }
+
+        SavePalindrome ->
+            { model
+                | currentPalindome = ""
+                , savedPalindromes =
+                    model.currentPalindome :: model.savedPalindromes
+            }
 
 
 view model =
     div []
-        [ input [ value model, onInput (\text -> text) ] []
+        [ input
+            [ value model.currentPalindome
+            , onInput UpdateCurrentPalindrome
+            ]
+            []
         , br [] []
-        , Html.text (toString (isPalindrome model))
+        , Html.text (toString (isPalindrome model.currentPalindome))
+        , br [] []
+        , button
+            [ onClick SavePalindrome ]
+            [ text "Save the palindrome" ]
+        , savedPalindromesView model.savedPalindromes
         ]
 
 
+savedPalindromesView palindromes =
+    ul []
+        (List.map palindromeView palindromes)
+
+
+palindromeView palindrome =
+    li [] [ text palindrome ]
+
+
 model =
-    "Are we not drawn onward, we few, drawn onward to new era?"
+    { currentPalindome = "Are we not drawn onward, we few, drawn onward to new era?"
+    , savedPalindromes = []
+    }
 
 
 isPalindrome input =

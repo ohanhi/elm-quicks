358b9c672324c8b4e694e69e71365886c988dd70
Daniel Landau <daniel@landau.fi>
2017-11-07 11:59:08 +0100

    Day 4

---
+++ day-04/Main.elm
@@ -0,0 +1,146 @@
+module Main exposing (..)
+
+import Char
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Json
+import Json.Encode as JS
+
+
+main =
+    Html.program
+        { view = view
+        , update = update
+        , init = init
+        , subscriptions = always Sub.none
+        }
+
+
+type Msg
+    = UpdateCurrentPalindrome String
+    | SavePalindrome
+    | GotPalindromes (List String)
+    | GotError Http.Error
+    | SaveOk
+
+
+update msg model =
+    case msg of
+        UpdateCurrentPalindrome text ->
+            { model | currentPalindome = text } ! []
+
+        SavePalindrome ->
+            let
+                newPalindromes =
+                    model.currentPalindome :: model.savedPalindromes
+            in
+                { model
+                    | currentPalindome = ""
+                    , savedPalindromes = newPalindromes
+                }
+                    ! [ put "http://localhost:3000/" (encode newPalindromes)
+                            |> Http.send
+                                (\response ->
+                                    case response of
+                                        Ok () ->
+                                            SaveOk
+
+                                        Err error ->
+                                            GotError error
+                                )
+                      ]
+
+        GotPalindromes palindromes ->
+            { model
+                | savedPalindromes = palindromes
+            }
+                ! []
+
+        GotError err ->
+            { model | errorMaybe = Just err } ! []
+
+        SaveOk ->
+            { model | errorMaybe = Nothing } ! []
+
+
+encode list =
+    list
+        |> List.map JS.string
+        |> JS.list
+
+
+put : String -> JS.Value -> Http.Request ()
+put url body =
+    Http.request
+        { method = "PUT"
+        , headers = []
+        , url = url
+        , body = Http.jsonBody body
+        , expect = Http.expectStringResponse (\_ -> Ok ())
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+
+view model =
+    div []
+        [ input
+            [ value model.currentPalindome
+            , onInput UpdateCurrentPalindrome
+            ]
+            []
+        , br [] []
+        , Html.text (toString (isPalindrome model.currentPalindome))
+        , br [] []
+        , button
+            [ onClick SavePalindrome ]
+            [ text "Save the palindrome" ]
+        , savedPalindromesView model.savedPalindromes
+        , br [] []
+        , model.errorMaybe
+            |> Maybe.map (\error -> Html.text (toString error))
+            |> Maybe.withDefault (Html.div [] [])
+        ]
+
+
+savedPalindromesView palindromes =
+    ul []
+        (List.map palindromeView palindromes)
+
+
+palindromeView palindrome =
+    li [] [ text palindrome ]
+
+
+init =
+    { currentPalindome = "Are we not drawn onward, we few, drawn onward to new era?"
+    , savedPalindromes = []
+    , errorMaybe = Nothing
+    }
+        ! [ Http.get "http://localhost:3000/" (Json.list Json.string)
+                |> Http.send
+                    (\response ->
+                        case response of
+                            Ok savedPalindromes ->
+                                GotPalindromes savedPalindromes
+
+                            Err error ->
+                                GotError error
+                    )
+          ]
+
+
+isPalindrome input =
+    let
+        characters =
+            String.toList (String.toLower input)
+
+        justLetters =
+            List.filter Char.isLower characters
+
+        lettersInReverse =
+            List.reverse justLetters
+    in
+        justLetters == lettersInReverse
